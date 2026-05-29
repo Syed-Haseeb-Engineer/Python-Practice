76d795405d1afc1b8323515cc565dde8ba8de020
Syed Haseeb <syedhasib2004@gmail.com>
2026-05-29 11:07:48 +0530

    session4

--- Python_For_AI_Practice/session4_tasks.py
+++ Python_For_AI_Practice/session4_tasks.py
@@ -163,99 +163,99 @@ print(result)
 # print("P14 Transpose:", [[row[i] for row in mat] for i in range(len(mat[0]))])
 # print("P15 Flatten:", [item for row in mat for item in row])
 
-# === SESSION 4: LISTS ===
+# # === SESSION 4: LISTS ===
 
-# P1: Combine two lists index-wise (with leftovers)
-l1 = ["M", "na", "i", "Kh"]
-l2 = ["y", "me", "s", "an", "Extra"]
-combined = []
-for i in range(max(len(l1), len(l2))):
-    temp = []
-    if i < len(l1): temp.append(l1[i])
-    if i < len(l2): temp.append(l2[i])
-    combined.append(temp)
-print("P1:", combined)
+# # P1: Combine two lists index-wise (with leftovers)
+# l1 = ["M", "na", "i", "Kh"]
+# l2 = ["y", "me", "s", "an", "Extra"]
+# combined = []
+# for i in range(max(len(l1), len(l2))):
+#     temp = []
+#     if i < len(l1): temp.append(l1[i])
+#     if i < len(l2): temp.append(l2[i])
+#     combined.append(temp)
+# print("P1:", combined)
 
-# P2: Add item to nested list after specified item
-list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
-# [CONCEPT UNLOCKED: list.index()]
-target_idx = list1[2][2].index(6000)
-list1[2][2].insert(target_idx + 1, 7000)
-print("P2:", list1)
+# # P2: Add item to nested list after specified item
+# list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+# # [CONCEPT UNLOCKED: list.index()]
+# target_idx = list1[2][2].index(6000)
+# list1[2][2].insert(target_idx + 1, 7000)
+# print("P2:", list1)
 
-# P3: Candy Inventory (Zip)
-candy = ['Jelly Belly', 'Kit Kat', 'Double Bubble', 'Milky Way', 'Three Musketeers']
-items = [10, 20, 34, 74, 32]
-print("P3:")
-for c, i in zip(candy, items):
-    print(f"{c}-{i}")
+# # P3: Candy Inventory (Zip)
+# candy = ['Jelly Belly', 'Kit Kat', 'Double Bubble', 'Milky Way', 'Three Musketeers']
+# items = [10, 20, 34, 74, 32]
+# print("P3:")
+# for c, i in zip(candy, items):
+#     print(f"{c}-{i}")
 
-# P4: Running Sum
-nums = [1, 2, 3, 4, 5, 6]
-run_sum = []
-total = 0
-for n in nums:
-    total += n
-    run_sum.append(total)
-print("P4:", run_sum)
+# # P4: Running Sum
+# nums = [1, 2, 3, 4, 5, 6]
+# run_sum = []
+# total = 0
+# for n in nums:
+#     total += n
+#     run_sum.append(total)
+# print("P4:", run_sum)
 
-# P5: Add all elements greater and itself
-nums2 = [2, 4, 6, 10, 1]
-res = [sum([y for y in nums2 if y >= x]) for x in nums2]
-print("P5:", res)
+# # P5: Add all elements greater and itself
+# nums2 = [2, 4, 6, 10, 1]
+# res = [sum([y for y in nums2 if y >= x]) for x in nums2]
+# print("P5:", res)
 
-# P6: Common unique items in increasing order (NO SETS ALLOWED YET)
-num1 = [23, 45, 67, 78, 89, 34, 34]
-num2 = [34, 89, 55, 56, 39, 67]
-common = []
-for n in num1:
-    if n in num2 and n not in common:
-        common.append(n)
-common.sort()
-print("P6:", common)
+# # P6: Common unique items in increasing order (NO SETS ALLOWED YET)
+# num1 = [23, 45, 67, 78, 89, 34, 34]
+# num2 = [34, 89, 55, 56, 39, 67]
+# common = []
+# for n in num1:
+#     if n in num2 and n not in common:
+#         common.append(n)
+# common.sort()
+# print("P6:", common)
 
-# P7: Sort alphanumeric strings based on product value
-strings = ['lac21', '23fg', '456', '098d', '1', 'kls']
-sorting_list = []
-for s in strings:
-    prod = 1
-    has_digit = False
-    for char in s:
-        if char.isdigit():
-            prod *= int(char)
-            has_digit = True
-    if not has_digit: prod = 1
-    sorting_list.append([prod, s])
-sorting_list.sort()
-print("P7:", [item[1] for item in sorting_list])
+# # P7: Sort alphanumeric strings based on product value
+# strings = ['lac21', '23fg', '456', '098d', '1', 'kls']
+# sorting_list = []
+# for s in strings:
+#     prod = 1
+#     has_digit = False
+#     for char in s:
+#         if char.isdigit():
+#             prod *= int(char)
+#             has_digit = True
+#     if not has_digit: prod = 1
+#     sorting_list.append([prod, s])
+# sorting_list.sort()
+# print("P7:", [item[1] for item in sorting_list])
 
-# P8: Split on space and hyphen
-raw = ['CampusX is a channel', 'for data-science', 'aspirants.']
-# Replace hyphen with space, then split the joined string
-p8_res = " ".join(raw).replace("-", " ").split()
-print("P8:", p8_res)
+# # P8: Split on space and hyphen
+# raw = ['CampusX is a channel', 'for data-science', 'aspirants.']
+# # Replace hyphen with space, then split the joined string
+# p8_res = " ".join(raw).replace("-", " ").split()
+# print("P8:", p8_res)
 
-# P9: Char Matrix to String (Comprehension)
-mat = [['c', 'a', 'm', 'p', 'u', 'x'], [' ', 'i', 's'], [' ', 'b', 'e', 's', 't']]
-p9_res = "".join([char for row in mat for char in row])
-print("P9:", p9_res)
+# # P9: Char Matrix to String (Comprehension)
+# mat = [['c', 'a', 'm', 'p', 'u', 'x'], [' ', 'i', 's'], [' ', 'b', 'e', 's', 't']]
+# p9_res = "".join([char for row in mat for char in row])
+# print("P9:", p9_res)
 
-# P10: Add Space before Capital Letters
-camel = ['campusxIs', 'bestFor', 'dataScientist']
-p10_res = ["".join([" " + c if c.isupper() else c for c in word]).strip() for word in camel]
-print("P10:", p10_res)
+# # P10: Add Space before Capital Letters
+# camel = ['campusxIs', 'bestFor', 'dataScientist']
+# p10_res = ["".join([" " + c if c.isupper() else c for c in word]).strip() for word in camel]
+# print("P10:", p10_res)
 
-# P11: Union of 2 lists
-l_a, l_b = [1,2,3,4,5,1], [2,3,5,7,8]
-union = l_a + [x for x in l_b if x not in l_a]
-print("P11:", union)
+# # P11: Union of 2 lists
+# l_a, l_b = [1,2,3,4,5,1], [2,3,5,7,8]
+# union = l_a + [x for x in l_b if x not in l_a]
+# print("P11:", union)
 
-# P12-P15: Matrix Comprehensions
-matrix = [[1,2,3], [4,5,6], [7,8,9]]
-print("P12 Row Max:", [max(row) for row in matrix])
-print("P13 Gen Matrix:", [[j for j in range(i, i+3)] for i in range(0, 9, 3)])
-print("P14 Transpose:", [[row[i] for row in matrix] for i in range(len(matrix[0]))])
-print("P15 Flatten:", [item for row in matrix for item in row])
+# # P12-P15: Matrix Comprehensions
+# matrix = [[1,2,3], [4,5,6], [7,8,9]]
+# print("P12 Row Max:", [max(row) for row in matrix])
+# print("P13 Gen Matrix:", [[j for j in range(i, i+3)] for i in range(0, 9, 3)])
+# print("P14 Transpose:", [[row[i] for row in matrix] for i in range(len(matrix[0]))])
+# print("P15 Flatten:", [item for row in matrix for item in row])
 
 
 
