91b72385398fe38d3616f17dbc5e143320ffd545
Syed Haseeb <syedhasib2004@gmail.com>
2026-06-04 18:26:35 +0530

    revision till session6

---
+++ Python_For_AI_Practice/session6_practice.py
@@ -0,0 +1,76 @@
+# def is_even(num):
+#     """
+#     This function returns if a given number is odd or even
+#     input - any valid integer
+#     output - odd/even
+#     created on - 16th Nov 2022
+#     """
+#     if type(num) == int:
+#         if num % 2 == 0:
+#             return 'even'
+#         else:
+#             return 'odd'
+#     else:
+#         return 'pagal hai kya?'
+
+# print(is_even.__doc__)
+
+# def g(y):
+#     print(x)
+#     print(x+1)
+# x = 5
+# g(x)
+# print(x)
+
+# def f(y):
+#     x = 1
+#     x += 1
+#     print(x)
+# x = 5
+# f(x)
+# print(x)
+
+# def h(y):
+#     x += 1
+# x = 5
+# h(x)
+# print(x)
+
+# def f(x):
+#     x = x + 1
+#     print('in f(x): x =', x)
+#     return x
+# x = 3
+# z = f(x)
+# print('in main program scope: z =', z)
+# print('in main program scope: x =', x)
+
+# def g(x):
+#     def h():
+#         x = 'abc'
+#     x = x + 1
+#     print('in g(x): x =',x)
+#     h()
+#     return x
+# x = 3
+# z = g(x)
+# print(x)
+# print(z)
+# def square(num):
+#   return num**2
+
+# type(square)
+
+# id(square)
+# L = [1,2,3,4,square]
+# print(L)
+# print(L[-1](3))
+# print(L)
+
+def f():
+    def x(a, b):
+        return a+b
+    return x
+    
+val = f()(3,4)
+print(val)
